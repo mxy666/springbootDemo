687f44b59ff94cdbc8a68d753dea8d9ccba0846d
mxy <785652792@qq.com>
2017-12-06 21:59:26 +0800

    创建权限实体类

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.security;
 
-import com.example.demo.filter.LoginFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,32 +7,52 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    //http://localhost:8080/login 输入正确的用户名密码 并且选中remember-me 则登陆成功，转到 index页面
+    //再次访问index页面无需登录直接访问
+    //访问http://localhost:8080/home 不拦截，直接访问，
+    //访问http://localhost:8080/hello 需要登录验证后，且具备 “ADMIN”权限hasAuthority("ADMIN")才可以访问
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/home").permitAll()//访问：/home 无需登录认证权限
+                .anyRequest().authenticated() //其他所有资源都需要认证，登陆后访问
+                .antMatchers("/hello").hasAuthority("ADMIN") //登陆后之后拥有“ADMIN”权限才可以访问/hello方法，否则系统会出现“403”权限不足的提示
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/login")//指定登录页是”/login”
                 .permitAll()
+                .successHandler(loginSuccessHandler()) //登录成功后可使用loginSuccessHandler()存储用户信息，可选。
                 .and()
                 .logout()
-                .permitAll();
+                .logoutSuccessUrl("/home") //退出登录后的默认网址是”/home”
+                .permitAll()
+                .invalidateHttpSession(true)
+                .and()
+                .rememberMe()//登录后记住用户，下次自动登录,数据库中必须存在名为persistent_logins的表
+                .tokenValiditySeconds(1209600);
     }
+
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws  Exception{
+
+        //指定密码加密所使用的加密器为passwordEncoder()
+        //需要将密码加密后写入数据库
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+        auth.eraseCredentials(false);
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(4);
+    }
 }
